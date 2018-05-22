cb2828f63a7a104e5de878351850c75b2f73be83
guham <guham.dev@gmail.com>
2018-05-21 23:47:27 -0500

    Updated vue-cli -> v3 beta 11

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,11 +1,18 @@
 module.exports = {
   root: true,
+  env: {
+    es6: true,
+    node: true
+  },
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb'
   ],
-  env: {
-    es6: true,
-    node: true
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
   }
 };

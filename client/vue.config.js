06ea9a37ee3a8c58949273285a598f95a86c9dd2
guham <guham.dev@gmail.com>
2018-06-07 15:09:33 -0500

    Added vscode vue.js debug

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   lintOnSave: false,
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === 'development') {
+      // Vue.js debugging in Chrome and VS Code
+      // https://github.com/Microsoft/vscode-recipes/tree/master/vuejs-cli
+      return {
+        devtool: 'source-map',
+      };
+    }
+    return {};
+  },
 };

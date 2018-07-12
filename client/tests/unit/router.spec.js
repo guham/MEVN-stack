ec8be3a5b214c09e702559ad48d5389753c6b1e5
guham <guham.dev@gmail.com>
2018-07-12 12:42:29 -0500

    Added router guard tests

--- client/tests/unit/router.spec.js
+++ client/tests/unit/router.spec.js
@@ -1,65 +1,89 @@
-import Vue from 'vue';
-import Router from 'vue-router';
 import router from '@/router';
-// components
+import store from '@/store';
 import Home from '@/views/Home.vue';
 import Test from '@/views/Test.vue';
 import FooPanel from '@/views/FooPanel.vue';
 import NotFound from '@/views/NotFound.vue';
 
-jest.mock('@/store', () => ({
-  default: jest.fn(),
-}));
-
-jest.mock('vue', () => ({
-  use: jest.fn(),
-}));
-
-jest.mock('vue-router', () => class VueRouter {
-  constructor(routes) {
-    const r = routes;
-    r.beforeEach = jest.fn();
-    return r;
-  }
+beforeEach(() => {
+  jest.resetModules();
+  jest.clearAllMocks();
 });
 
-describe('Router', () => {
-  test('Vue should use router', () => {
-    expect(Vue.use).toHaveBeenCalledWith(Router);
-  });
+jest.mock('@/store', () => ({
+  default: jest.fn(),
+  getters: {
+    get 'user/isAuthenticated'() {
+      return false;
+    },
+  },
+}));
 
-  test('is in mode `history`', () => {
+describe('Router', () => {
+  const { routes } = router.options;
+
+  test('is in `history` mode', () => {
     expect(router.mode).toBe('history');
   });
 
   test('should declare Home route', () => {
-    expect(router.routes[0].path).toEqual('/');
-    expect(router.routes[0].name).toEqual('Home');
-    expect(router.routes[0].component).toEqual(Home);
+    expect(routes[0].path).toEqual('/');
+    expect(routes[0].name).toEqual('Home');
+    expect(routes[0].component).toEqual(Home);
   });
 
   test('should declare Test route', () => {
-    expect(router.routes[1].path).toEqual('/test');
-    expect(router.routes[1].name).toEqual('Test');
-    expect(router.routes[1].component).toEqual(Test);
-    expect(router.routes[1].meta.requiresAuth).toBeTruthy();
+    expect(routes[1].path).toEqual('/test');
+    expect(routes[1].name).toEqual('Test');
+    expect(routes[1].component).toEqual(Test);
+    expect(routes[1].meta.requiresAuth).toBeTruthy();
   });
 
   test('should declare FooPanel route', () => {
-    expect(router.routes[2].path).toEqual('/foo');
-    expect(router.routes[2].name).toEqual('FooPanel');
-    expect(router.routes[2].component).toEqual(FooPanel);
-    expect(router.routes[2].meta.requiresAuth).toBeTruthy();
+    expect(routes[2].path).toEqual('/foo');
+    expect(routes[2].name).toEqual('FooPanel');
+    expect(routes[2].component).toEqual(FooPanel);
+    expect(routes[2].meta.requiresAuth).toBeTruthy();
   });
 
   test('should declare NotFound route', () => {
-    expect(router.routes[3].path).toEqual('/404');
-    expect(router.routes[3].name).toEqual('NotFound');
-    expect(router.routes[3].component).toEqual(NotFound);
+    expect(routes[3].path).toEqual('/404');
+    expect(routes[3].name).toEqual('NotFound');
+    expect(routes[3].component).toEqual(NotFound);
   });
 
   test('should declare redirect to 404 route', () => {
-    expect(router.routes[4].path).toEqual('*');
-    expect(router.routes[4].redirect).toEqual('/404');
+    expect(routes[4].path).toEqual('*');
+    expect(routes[4].redirect).toEqual('/404');
+  });
+
+  test('should be able to navigate to unprotected page without authentication', () => {
+    router.push('/');
+    expect(router.history.current.path).toBe('/');
+    expect(router.getMatchedComponents('/')[0].name).toBe('Home');
+    router.push('/404');
+    expect(router.history.current.path).toBe('/404');
+    expect(router.getMatchedComponents('/404')[0].name).toBe('NotFound');
+  });
+
+  test('should not be able to navigate to protected page when not authenticated', () => {
+    router.push('/test');
+    expect(router.history.current.path).toBe('/');
+  });
+
+  test('should be able to navigate to protected page when authenticated', () => {
+    Object.defineProperty(store.getters, 'user/isAuthenticated', {
+      get: jest.fn(() => true),
+    });
+    router.push('/foo');
+    expect(router.history.current.path).toBe('/foo');
+  });
+
+  test('should be able to navigate to unprotected page when authenticated', () => {
+    Object.defineProperty(store.getters, 'user/isAuthenticated', {
+      get: jest.fn(() => true),
+    });
+    router.push('/');
+    expect(router.history.current.path).toBe('/');
   });
 });

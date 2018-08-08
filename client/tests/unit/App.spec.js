cba89f1ab87139a3f143a919c6bc0b51e7f09d12
guham <guham.dev@gmail.com>
2018-08-08 17:03:45 -0500

    Updated App.vue snapshots tests

--- client/tests/unit/App.spec.js
+++ client/tests/unit/App.spec.js
@@ -3,23 +3,40 @@ import App from '@/App.vue';
 import user from '@/store/modules/user';
 import factory from './factory';
 
-describe('App.vue', () => {
-  const store = new Vuex.Store({
-    modules: {
-      user: {
-        namespaced: user.namespaced,
-        getters: {
-          isAuthenticated: () => false,
-        },
+let store = new Vuex.Store({
+  modules: {
+    user: {
+      namespaced: user.namespaced,
+      getters: {
+        isAuthenticated: () => false,
       },
     },
-  });
+  },
+});
+
+describe('App.vue', () => {
   test('#app is define', () => {
     const wrapper = factory(App, store);
     expect(wrapper.find('app')).toBeDefined();
   });
 
-  test('has the expected html structure', () => {
+  test('should display only "Home" link when the user is not authenticated', () => {
+    const wrapper = factory(App, store);
+    const template = wrapper.html();
+    expect(template).toMatchSnapshot();
+  });
+
+  test('should display restricted links when the user is authenticated', () => {
+    store = new Vuex.Store({
+      modules: {
+        user: {
+          namespaced: user.namespaced,
+          getters: {
+            isAuthenticated: () => true,
+          },
+        },
+      },
+    });
     const wrapper = factory(App, store);
     const template = wrapper.html();
     expect(template).toMatchSnapshot();

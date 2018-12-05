578a61984c6e4eb5a5b06d2311f9b1da6a41ab63
guham <guham.dev@gmail.com>
2018-12-05 20:09:59 +0100

    Improved unit/mutation tests: debug

--- api/src/utils/debug.js
+++ api/src/utils/debug.js
@@ -3,6 +3,8 @@ const debug = require('debug');
 module.exports = ({ parameters }) => (ns) => {
   if (parameters.app.isInEnv('test')) {
     debug.disable();
+  } else {
+    debug.enable(ns);
   }
   return debug(ns);
 };
--- api/tests/unit/utils/debug.test.js
+++ api/tests/unit/utils/debug.test.js
@@ -1,7 +1,23 @@
-const debug = require('../../../src/utils/debug')('env-test');
+const debug = require('../../../src/utils/debug');
+
+const parameters = {
+  app: {
+    isInEnv: jest.fn(),
+  },
+};
 
 describe('Test debugging utility', () => {
   test('Should be disabled in test env', () => {
-    expect(debug.enabled).toBeFalsy();
+    parameters.app.isInEnv.mockReturnValueOnce(true);
+    const { enabled } = debug({ parameters })('test');
+    expect(enabled).toBeFalsy();
+    expect(parameters.app.isInEnv).toHaveBeenCalledWith('test');
+  });
+
+  test('Should be enabled otherwise', () => {
+    parameters.app.isInEnv.mockReturnValueOnce(false);
+    const { enabled } = debug({ parameters })('dev-or-prod');
+    expect(enabled).toBeTruthy();
+    expect(parameters.app.isInEnv).toHaveBeenCalledWith('test');
   });
 });

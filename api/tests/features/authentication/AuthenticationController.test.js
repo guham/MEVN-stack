a054194c74f1eb8c9ef1b04787f31c9086e0c38d
guham <guham.dev@gmail.com>
2018-12-14 17:24:53 +0100

    Improved integration tests: authentication controller

--- api/tests/features/authentication/AuthenticationController.test.js
+++ api/tests/features/authentication/AuthenticationController.test.js
@@ -9,18 +9,20 @@ describe('Test authentication routes', () => {
         getPayload: jest.fn(() => (getDefaultUserPayload())),
       })));
       const idToken = 'google-id-token';
-      const verifyIdTokenOptions = {
-        idToken,
-        audience: process.env.GOOGLE_OAUTH_CLIENT_ID,
-      };
 
-      const response = await request().post('/auth/tokens').send({ idToken });
-      expect(response.statusCode).toBe(200);
+      const response = await request().post('/auth/tokens').send({ idToken }).expect(200);
       expect(Object.keys(response.body)).toEqual(['accessToken', 'refreshToken', 'expirationDate']);
       expect(response.body.accessToken).toBeDefined();
       expect(response.body.refreshToken).toBeDefined();
       expect(response.body.expirationDate).toBeDefined();
-      expect(OAuth2Client.prototype.verifyIdToken).toHaveBeenCalledWith(verifyIdTokenOptions);
+    });
+
+    test('Should return the error with status 500 if something wrong happens', async () => {
+      OAuth2Client.prototype.verifyIdToken = jest.fn(() => Promise.reject(new Error('Some authentication error')));
+      const idToken = 'google-id-token';
+
+      const response = await request().post('/auth/tokens').send({ idToken }).expect(500);
+      expect(response.body.message).toBe('Some authentication error');
     });
   });
 });
